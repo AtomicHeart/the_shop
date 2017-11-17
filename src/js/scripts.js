33241824e44701826089cd826ab8b7bb266e580f
RhytmGuitar <maxim.khalilov@gmail.com>
2017-11-17 18:39:57 +0300

    Made an adaptive footer.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -37,3 +37,35 @@ product.addEventListener('click', (mouseEvent) => {
     }
 }
 );
+
+const support = document.getElementById('support');
+const services = document.getElementById('services');
+const social = document.getElementById('social');
+
+let socialWrapped = false;
+let socialWrapped2 = false;
+let servicesWrapped = false;
+
+window.addEventListener('resize', () => {
+    social.classList.remove('wrapped');
+
+    if(support.getBoundingClientRect().top < services.getBoundingClientRect().top)
+        servicesWrapped = true;
+    else
+        servicesWrapped = false;
+
+    if(support.getBoundingClientRect().top < social.getBoundingClientRect().top)
+        socialWrapped = true;
+    else
+        socialWrapped = false;
+
+    if(services.getBoundingClientRect().top < social.getBoundingClientRect().top)
+        socialWrapped2 = true;
+    else
+        socialWrapped2 = false;
+
+    if(socialWrapped2 || (socialWrapped && !servicesWrapped)) {
+        social.classList.add('wrapped');
+    }
+}
+);
