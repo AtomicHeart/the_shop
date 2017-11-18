1ac026fce6a2183b4dc6f4e053ecde6bbbfe9eef
RhytmGuitar <maxim.khalilov@gmail.com>
2017-11-19 00:35:26 +0300

    Added memorization of properties in local storage. Fix wrap detector.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,6 +5,13 @@ const previewImage = document.getElementById('previewImage');
 const colorNames = ['white', 'yellow', 'green'];
 const imagesDir = 'img/tshirts';
 const productImgName = 'tshirt';
+const productId = 978123;
+
+const properties = localStorage.getItem('p' + productId);
+let propertiesObject = {};
+if(properties){
+    propertiesObject = JSON.parse(properties);
+}
 
 new PropertySelector(document.getElementById('colorList'), product);
 new PropertySelector(document.getElementById('sizeList'), product);
@@ -13,10 +20,12 @@ product.addEventListener('property-selected', event => {
     const data = event.detail;
 
     if (data.type === 'color') {
+        remeberProductProperty('color', data.value);
         changePicture(data.value);
     }
 
     if (data.type === 'size') {
+        remeberProductProperty('size', data.value);
         changePrice();
     }
 });
@@ -29,14 +38,44 @@ function changePicture(color) {
     previewImage.setAttribute('src', imagesDir + '/' + productImgName + '_' + colorNames[color] + '.jpg');
 }
 
+function remeberProductProperty(property, value){
+    propertiesObject[property] = value;
+    console.log(property, value);
+}
+
 product.addEventListener('click', (mouseEvent) => {
-    if (mouseEvent.target.hasAttribute('favorite')) {
-        mouseEvent.target.removeAttribute('favorite');
-    } else {
-        mouseEvent.target.setAttribute('favorite', '');
+    if (mouseEvent.target.getAttribute('id') === 'favoriteButton') {
+        if (mouseEvent.target.hasAttribute('favorite')) {
+            remeberProductProperty('favorite', false);
+            mouseEvent.target.removeAttribute('favorite');
+        }
+        else {
+            remeberProductProperty('favorite', true);
+            mouseEvent.target.setAttribute('favorite', '');
+        }
     }
 }
 );
+
+function saveToLocalStorage(){
+    localStorage.setItem('p' + productId, JSON.stringify(propertiesObject));
+}
+
+window.addEventListener('beforeunload', saveToLocalStorage);
+
+if(propertiesObject.color !== undefined) {
+    document.getElementById('color_' + propertiesObject.color).checked = true;
+    changePicture(propertiesObject.color);
+}
+
+if(propertiesObject.size !== undefined) {
+    document.getElementById('size_' + propertiesObject.size).checked = true;
+}
+
+if(propertiesObject.favorite !== undefined) {
+    if(propertiesObject.favorite)
+        document.getElementById('favoriteButton').setAttribute('favorite', '');
+}
 
 const support = document.getElementById('support');
 const services = document.getElementById('services');
@@ -46,7 +85,7 @@ let socialWrapped = false;
 let socialWrapped2 = false;
 let servicesWrapped = false;
 
-window.addEventListener('resize', (() => {
+function wrapManager() {
     social.classList.remove('wrapped');
 
     if(support.getBoundingClientRect().top < services.getBoundingClientRect().top)
@@ -67,5 +106,7 @@ window.addEventListener('resize', (() => {
     if(socialWrapped2 || (socialWrapped && !servicesWrapped)) {
         social.classList.add('wrapped');
     }
-})()
-);
+}
+
+window.addEventListener('resize', wrapManager);
+wrapManager();
